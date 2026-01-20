5670d2304e29c0d48996740fa5b29f2d63293a76
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 19:46:21 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fee2e2',
+          100: '#fecaca',
+          500: '#ef4444',
+          600: '#dc2626',
+          700: '#b91c1c'
+        }
+      }
+    }
+  },
+  plugins: []
+}
