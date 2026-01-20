3e34916e54c0d77804cfcf144621462e7fc35e3c
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 17:56:35 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <nav>
+      {/* Add Navbar content here with appropriate Links */}
+    </nav>
+  )
+}
+
+export default Navbar
