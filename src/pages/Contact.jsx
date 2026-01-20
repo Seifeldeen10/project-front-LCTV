07b06e885090ed65cfe40e0aef843090f703e71f
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 20:09:07 +0200

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Contact(){
+    return(
+        <div className="bg-primary-500 flex justify-center items-center h-screen text-2xl text-white">
+            Contact Page
+        </div>
+)
+}
+export default Contact
