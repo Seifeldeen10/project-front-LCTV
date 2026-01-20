9a32ef85c569a6d314b383ba0d404355d0d5ac79
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 20:09:06 +0200

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function About(){
+    return(
+        <div className="bg-primary-500 flex justify-center items-center h-screen text-2xl text-white">
+            About Page
+        </div>
+)
+}
+export default About;
