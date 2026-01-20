33d52e99a5960911093a2dd2d645f65a80f1eba6
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 20:09:08 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Footer(){
+    return(
+        <div className="bg-primary-500 w-full pin-b p-4">
+            <p className="text-center text-white">2023 - React App</p>
+        </div>
+)
+}
+export default Footer
