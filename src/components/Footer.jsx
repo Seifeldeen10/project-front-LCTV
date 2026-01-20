116150254321fa952ad5fdda6b12f6c807bc5383
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 19:13:46 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='bg-primary-500 text-primary-600'>
+      <div className='container mx-auto px-4 py-8'>
+        <h2 className='text-3xl font-bold text-gray-600 mb-8'>Footer</h2>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
