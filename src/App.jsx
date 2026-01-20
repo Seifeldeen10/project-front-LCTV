d5b0d8245892a5fd2ffa7fbbc63a5bd597c649b9
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 16:06:42 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-primary-600 mb-4">
+          Welcome to LVC Website Project
+        </h1>
+        <p className="text-gray-700">
+          Your React + Vite + Tailwind CSS application is ready!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
