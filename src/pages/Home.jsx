fe107e9fdfacd45da1efcd50ffd02f7c9b04d3d1
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 20:09:06 +0200

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Home(){
+    return(
+        <div className="bg-primary-500 flex justify-center items-center h-screen text-2xl text-white">
+             Home Page
+        </div>
+)
+}
+export default Home;
