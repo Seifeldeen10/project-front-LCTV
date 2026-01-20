300f6428107cf1e9a599e1d030e4903f0a2dfd2e
Seif abdelaty <seifeldeen2011@gmail.com>
2026-01-20 20:09:09 +0200

    initial commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function HeroSection(){
+    return(
+        <div className="bg-primary-500 flex justify-center items-center h-screen text-2xl text-white">
+            Hero Section
+        </div>
+)
+}
+export default HeroSection
